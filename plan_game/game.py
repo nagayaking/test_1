d13dc79fca67686b3a9a308601afea678c9abdee
nagaya-king <hhaarruuttoo31415@gmail.com>
2025-06-23 18:53:20 +0900

    判定を追加しようとしている

--- plan_game/game.py
+++ plan_game/game.py
@@ -1,10 +1,12 @@
 import pyxel
 
-SCREEN_WIDTH = 320
-SCREEN_HEIGHT = 240
+SCREEN_WIDTH = 160
+SCREEN_HEIGHT = 120
 GROUND_HEIGHT = SCREEN_HEIGHT * 4 //5
 FLYING_TIME = 0
 GRAVITATIONA_ACCELERATION = 0.1
+
+check = [(0,0),(0,7),(7,0),(7,7)]
 
 def get_tile(tile_x, tile_y):
     return pyxel.tilemaps[0].pget(tile_x, tile_y)
@@ -14,7 +16,7 @@ class App:
         pyxel.init(SCREEN_WIDTH,SCREEN_HEIGHT,title="game")
         pyxel.load("me.pyxres")
         self.player_x = 0
-        self.player_y = GROUND_HEIGHT - 16
+        self.player_y = GROUND_HEIGHT - 8
         self.flying_timer = FLYING_TIME
         self.accelerate = GRAVITATIONA_ACCELERATION
         self.right = True
@@ -26,12 +28,19 @@ class App:
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
 
+        
+        for cpx, cpy in check:
+            xi = (self.player_x + cpx) // 8
+            xy = (self.player_y + cpy) // 8
+
+        tile = pyxel.tilemap(0).pget()
+
         # 落下
-        if self.player_y + 16 < GROUND_HEIGHT and not self.is_flying:
+        if self.player_y + 8 < GROUND_HEIGHT and not self.is_flying:
             self.player_y += 2
 
         # ジャンプ
-        if pyxel.btn(pyxel.KEY_W) and GROUND_HEIGHT - self.player_y == 16:
+        if pyxel.btn(pyxel.KEY_W) and GROUND_HEIGHT - self.player_y == 8:
             self.is_flying = True
         if self.is_flying:
             self.flying_timer += 1
@@ -44,20 +53,20 @@ class App:
 
         # プレイヤーの移動
         if pyxel.btn(pyxel.KEY_A) and self.player_x > 0:
-            self.player_x -= 2
+            self.player_x -= 1
             self.right = False
-        elif pyxel.btn(pyxel.KEY_D) and self.player_x + 16 < SCREEN_WIDTH:
-            self.player_x += 2
+        elif pyxel.btn(pyxel.KEY_D) and self.player_x + 8 < SCREEN_WIDTH:
+            self.player_x += 1
             self.right = True
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_GRAY)
-        pyxel.rect(0, GROUND_HEIGHT, SCREEN_WIDTH, 10, pyxel.COLOR_GREEN)
+        pyxel.bltm(0, 0, 0, self.player_x, 0, 160, 120, pyxel.COLOR_BLACK)
 
         #キャラの向き
         if self.right:
-            pyxel.blt(self.player_x, self.player_y, 0, 8, 0, 16, 16, pyxel.COLOR_GRAY)
+            pyxel.blt(self.player_x, self.player_y, 0, 0, 16, 8, 8, pyxel.COLOR_GRAY)
         else:
-            pyxel.blt(self.player_x, self.player_y, 0, 8, 0, -16, 16, pyxel.COLOR_GRAY)
+            pyxel.blt(self.player_x, self.player_y, 0, 0, 16, -8, 8, pyxel.COLOR_GRAY)
 
 App()
