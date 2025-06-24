bb3c1029491a644eb9bf3e41f00ddd6f8ddff716
nagaya-king <hhaarruuttoo31415@gmail.com>
2025-06-24 17:49:59 +0900

    壁の当たり判定の前

--- plan_game/game.py
+++ plan_game/game.py
@@ -3,8 +3,12 @@ import pyxel
 SCREEN_WIDTH = 160
 SCREEN_HEIGHT = 120
 GROUND_HEIGHT = SCREEN_HEIGHT * 4 //5
-FLYING_TIME = 0
-GRAVITATIONA_ACCELERATION = 0.1
+LEFT_LINE = 40
+RIGHT_LINE = SCREEN_WIDTH - 48
+STAGE_WIDTH = 128 * 3
+STAGE_HEIGHT = 128 * 1
+scroll_x = 0
+scroll_y = 0
 
 
 check = [(0,0),(0,7),(7,0),(7,7)]
@@ -21,36 +25,46 @@ class App:
         self.player_y = GROUND_HEIGHT - 8
         self.player_dy = 0
         self.jump = 0
-        self.flying_timer = FLYING_TIME
-        self.accelerate = GRAVITATIONA_ACCELERATION
         self.right = True
         self.is_flying = False
         pyxel.run(self.update,self.draw)
 
-            
 
 
     def update(self):
-        if pyxel.btnp(pyxel.KEY_ESCAPE):
-            pyxel.quit()
+        global scroll_x,scroll_y
+
+        # 左方向へのスクロール
+        if self.player_x < scroll_x + LEFT_LINE:
+            scroll_x = self.player_x - LEFT_LINE
+            if scroll_x < 0:
+                scroll_x = 0
+
+        # 右方向へのスクロール
+        if scroll_x + RIGHT_LINE < self.player_x:
+            scroll_x = self.player_x - RIGHT_LINE
+            if SCREEN_WIDTH - pyxel.width < scroll_x:
+                scroll_x = STAGE_WIDTH - pyxel.width
+
+        print(self.player_x, scroll_x, pyxel.width)
 
         def chkwall(self,cx,cy):
             c = 0
             if pyxel.height < cy:
-                c = c + 1                  # 画面下に落ちたところで止める
+                c = c + 1               # 画面下に落ちたところで止める
             for cpx,cpy in check:
                 xi = (cx + cpx)//8
                 yi = (cy + cpy)//8
-                if ((5,0) == pyxel.tilemap(0).pget(xi,yi) or
-                    (6,0) == pyxel.tilemap(0).pget(xi,yi) or
-                    (6,1) == pyxel.tilemap(0).pget(xi,yi)):
+                if (5,0) == pyxel.tilemap(0).pget(xi,yi):
                     c = c + 1
+
+            print(c)
             return c
 
         if self.jump == 0:
             if chkwall(self,self.player_x,self.player_y+1) == 0:
                 self.jump = 2  # 床が無ければ落下
-            if pyxel.btnp(pyxel.KEY_SPACE):
+            if pyxel.btnp(pyxel.KEY_W):
                 self.player_dy = 8
                 self.jump = 1   # ジャンプ開始
         else:
@@ -81,12 +95,18 @@ class App:
             self.player_x += 1
             self.player_dx = 1
             self.right = True
+    
 
-        print(self.jump, self.player_x, self.player_y)
+        # 終了
+        if pyxel.btnp(pyxel.KEY_ESCAPE) or self.player_y >= SCREEN_HEIGHT:
+            pyxel.quit()
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_GRAY)
-        pyxel.bltm(0, 0, 0, self.player_x, 0, 160, 120, pyxel.COLOR_BLACK)
+        pyxel.camera()
+        pyxel.bltm(0,0, 0, scroll_x, scroll_y, pyxel.width,pyxel.height, 0)
+
+        pyxel.camera(scroll_x,scroll_y)
 
         #キャラの向き
         if self.right:
