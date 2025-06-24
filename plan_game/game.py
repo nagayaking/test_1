70482419cde4ef4d64d1f5542f25004883637fd7
nagaya-king <hhaarruuttoo31415@gmail.com>
2025-06-24 16:21:43 +0900

    当たり判定が少し変

--- plan_game/game.py
+++ plan_game/game.py
@@ -6,6 +6,7 @@ GROUND_HEIGHT = SCREEN_HEIGHT * 4 //5
 FLYING_TIME = 0
 GRAVITATIONA_ACCELERATION = 0.1
 
+
 check = [(0,0),(0,7),(7,0),(7,7)]
 
 def get_tile(tile_x, tile_y):
@@ -19,25 +20,13 @@ class App:
         self.player_dx = 0
         self.player_y = GROUND_HEIGHT - 8
         self.player_dy = 0
+        self.jump = 0
         self.flying_timer = FLYING_TIME
         self.accelerate = GRAVITATIONA_ACCELERATION
         self.right = True
         self.is_flying = False
         pyxel.run(self.update,self.draw)
 
-
-    def chkwall(self,cx,cy):
-        c = 0
-        if cx < 0 or pyxel.width -8 < cx:
-            c = c + 1
-        if pyxel.height < cy:
-            c = c + 1                  # 画面下に落ちたところで止める
-        for cpx,cpy in check:
-            xi = (cx + cpx)//8
-            yi = (cy + cpy)//8
-            if (1,0) == pyxel.tilemap(0).pget(xi,yi):
-                c = c + 1
-        return c
             
 
 
@@ -45,22 +34,42 @@ class App:
         if pyxel.btnp(pyxel.KEY_ESCAPE):
             pyxel.quit()
 
+        def chkwall(self,cx,cy):
+            c = 0
+            if pyxel.height < cy:
+                c = c + 1                  # 画面下に落ちたところで止める
+            for cpx,cpy in check:
+                xi = (cx + cpx)//8
+                yi = (cy + cpy)//8
+                if ((5,0) == pyxel.tilemap(0).pget(xi,yi) or
+                    (6,0) == pyxel.tilemap(0).pget(xi,yi) or
+                    (6,1) == pyxel.tilemap(0).pget(xi,yi)):
+                    c = c + 1
+            return c
 
+        if self.jump == 0:
+            if chkwall(self,self.player_x,self.player_y+1) == 0:
+                self.jump = 2  # 床が無ければ落下
+            if pyxel.btnp(pyxel.KEY_SPACE):
+                self.player_dy = 8
+                self.jump = 1   # ジャンプ開始
+        else:
+            self.player_dy = self.player_dy - 1
+            if self.player_dy < 0:
+                self.jump = 2    # 頂点で落下開始
 
-        # 落下
-        if self.player_y + 8 < GROUND_HEIGHT and not self.is_flying:
-            self.player_y += 2
-
-        # ジャンプ
-        if pyxel.btn(pyxel.KEY_W) and GROUND_HEIGHT - self.player_y == 8:
-            self.is_flying = True
-        if self.is_flying:
-            self.flying_timer += 1
-            self.player_y -= 4
-            if self.flying_timer == 10:
-                self.is_flying = False
-                self.flying_timer = FLYING_TIME
-                
+        ud = pyxel.sgn(self.player_dy)
+        loop = abs(self.player_dy)
+        while 0 < loop :
+            if chkwall(self,self.player_x, self.player_y - ud) != 0:
+                self.player_dy = 0
+                if self.jump == 1:
+                    self.jump = 2   # 壁にぶつかって落下
+                elif self.jump == 2:
+                    self.jump = 0   # 着地　落下終了
+                break
+            self.player_y = self.player_y - ud
+            loop = loop -1
 
 
         # プレイヤーの移動
@@ -72,6 +81,8 @@ class App:
             self.player_x += 1
             self.player_dx = 1
             self.right = True
+
+        print(self.jump, self.player_x, self.player_y)
 
     def draw(self):
         pyxel.cls(pyxel.COLOR_GRAY)
