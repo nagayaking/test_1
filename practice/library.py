addc33cd79c5c74cec7c1ac99af629aa50f6c453
nagaya-king <hhaarruuttoo31415@gmail.com>
2025-07-04 18:52:55 +0900

    なんか動かなくなった

--- practice/library.py
+++ practice/library.py
@@ -1,15 +1,24 @@
 import datetime
 
+#並び替え関数
 def list_sort(lis_join, file):
     words_base = []
     words = []
+    new_words_list = []         
     new_words = ""
     dic = {}
     num = 0
 
+    
+
     with open(file, "r") as f:
         words_base = f.readlines()
-    words_base.append(lis_join)
+    if lis_join != "" or lis_join != []:
+        if type(lis_join) == "str":
+           words_base.append(lis_join)
+        elif type(lis_join) == "list":
+            for i in range(len(lis_join)):
+                words_base.append(lis_join)
 
     #リストに整える
     for i in words_base:
@@ -30,8 +39,51 @@ def list_sort(lis_join, file):
     #元に戻す
     for _, task_list in dic_sorted:
         new_word = "|".join(task_list)
+        new_words_list.append(new_word)
         new_words = new_words + ("\n" if new_words != "" else "") + new_word
     with open(file, "w") as f:
         f.write(new_words)
 
-    return new_words
+    return new_words_list
+
+
+#履歴作成関数
+def make_log(current_file, log_file):
+
+    live_tasks = []
+    dead_tasks = []
+
+
+    with open("task_log.txt") as f:
+        dead = f.read()
+
+    #現在のシリアル値を取得
+    dt = str(datetime.datetime.now())
+    dt = dt[:10]
+    dt_list = dt.split("-")
+    now_serial = datetime.datetime(int(dt_list[0]), int(dt_list[1]), int(dt_list[2])) - datetime.datetime(1899, 12, 31)
+
+    with open(current_file, "r") as f:
+        tasks_base = f.readlines()
+
+    #リストに整える
+    for i in tasks_base:
+        if i.endswith("\n"):
+            i_1 = i.strip()
+            a = i_1.split("|")
+        else:
+            a = i.split("|")
+
+        #シリアル値で分類
+        num = datetime.datetime(int(a[1]), int(a[2]), int(a[3])) - datetime.datetime(1899, 12, 31)
+        serial = num.days
+        if int(serial) < int(now_serial.days):
+            dead_tasks.append(i)
+        else:
+            live_tasks.append(i)
+            with open(current_file, "w") as f:
+                f.writelines(live_tasks)
+
+    list_sort(dead_tasks, log_file)
+
+list_sort("fjjfdjf|2027|4|5", "task.txt")
--- practice/task.py
+++ practice/task.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 import datetime 
 import calendar
 
-from library import list_sort
+import library
 
 selected_value_year = 1
 selected_value_month = 1
@@ -12,10 +12,10 @@ dates = []
 
 #ひと月の長さ
 def month_range(year,month):
-    day, range = calendar.monthrange(year, month)
+    _, range = calendar.monthrange(year, month)
     return range
 with open("task.txt", "r") as f:
-    taskss = f.read()
+    taskss = f.readlines()
 
 
 #現在の年月日を取得
@@ -26,7 +26,6 @@ month = current.month
 date = current.date
 
 #曜日と年のリスト
-days = {0:"月曜日", 1:"火曜日", 2:"水曜日", 3:"木曜日", 4:"金曜日", 5:"土曜日", 6:"日曜日"}
 years = []
 for i in range(year,year + 11):
     years.append(i)
@@ -35,6 +34,10 @@ for i in range(year,year + 11):
 root = tk.Tk()
 root.title(u"my_task")
 root.geometry("400x300")
+library.make_log("task.txt", "task_log.txt")
+with open("task_log.txt") as f:
+    log = f.readlines()
+
 
 #ラベル
 Static1 = tk.Label(text=u"タスクを入力")
@@ -42,18 +45,39 @@ Static2 = tk.Label(text=u"年を入力")
 Static3 = tk.Label(text=u"月を入力")
 Static4 = tk.Label(text=u"日付を入力")
 Static5 = tk.Label(text=u"現在のタスク")
-list_tasks =tk.Label(text=taskss,background="#f0f8ff")
+Static6 = tk.Label(text=u"タスクの履歴")
 
+#リストの選択
+list_tasks = tk.StringVar(value=tuple(taskss))
+list_log = tk.StringVar(value=tuple(log))
+
+#各種ウィジェットの作成
+Listbox1 = tk.Listbox(root, listvariable=list_tasks, height=5)
+Listbox2 = tk.Listbox(root, listvariable=list_log,height=5)
+
+#スクロールバーの作成
+scrollbar1 = tk.Scrollbar(root, orient=tk.VERTICAL, command=Listbox1.yview)
+scrollbar2 = tk.Scrollbar(root, orient=tk.VERTICAL, command=Listbox2.yview)
+
+ # スクロールバーをListboxに反映
+Listbox1["yscrollcommand"] = scrollbar1.set
+Listbox2["yscrollcommand"] = scrollbar2.set
+
+#グリッドの設置
 Static1.grid(row=0, column=0, columnspan=2, sticky = tk.N+tk.S)
 Static2.grid(row=1, column=0, columnspan=2, sticky = tk.N+tk.S)
 Static3.grid(row=2, column=0, columnspan=2, sticky = tk.N+tk.S)
 Static4.grid(row=3, column=0, columnspan=2, sticky = tk.N+tk.S)
 Static5.grid(row=6,column=0)
-list_tasks.grid(row=7, column=0, columnspan=5, sticky=tk.W, padx=10)
+Static6.grid(row=6,column=2)
+Listbox1.grid(row=7, column=0)
+scrollbar1.grid(row=7, column=1, sticky=(tk.N, tk.S))
+Listbox2.grid(row=7, column=2)
+scrollbar2.grid(row=7, column=3, sticky=(tk.N, tk.S))
 
 #エントリー
-Editbox =tk.Entry()
-Editbox.grid(row=0, column=3)
+Editbox = tk.Entry()
+Editbox.grid(row=0, column=2)
 
 value = 0
 def get_value():
@@ -71,7 +95,6 @@ def on_select(event):
 
     #月を取得
     months = ["1","2","3","4","5","6","7","8","9","10","11","12"]
-    rng = 0
     combobox_2 = ttk.Combobox(root, values = months)
     def on_select(event):
         global selected_value_month, dates
@@ -89,30 +112,34 @@ def on_select(event):
             print("選択された値:", selected_value_date)
             print(f"選択した日付は{selected_value_year}年{selected_value_month}月{selected_value_date}日です")
         combobox_3.bind("<<ComboboxSelected>>", on_select)
-        combobox_3.grid(row=3,column=3)
+        combobox_3.grid(row=3,column=2)
 
     combobox_2.bind("<<ComboboxSelected>>", on_select)
-    combobox_2.grid(row=2,column=3)
+    combobox_2.grid(row=2,column=2)
 
 combobox_1.bind("<<ComboboxSelected>>", on_select)
-combobox_1.grid(row=1,column=3)
+combobox_1.grid(row=1,column=2)
 
 
 #送信ボタンと更新
 def button_clicked():
-    global tasks, taskss
-    value = get_value()
+    global tasks
+    selected_value_task = get_value()
     with open("task.txt", "r") as f:
         tasks = f.readlines()
-    join_task = f"{value}|{selected_value_year}|{selected_value_month}|{selected_value_date}"
-    new_tasks = list_sort(join_task, "task.txt")
-    list_tasks =tk.Label(text=new_tasks,background="#f0f8ff")
-    list_tasks.grid(row=7, column=0, columnspan=5, sticky=tk.W, padx=10)
-    
- 
+    join_task = f"{selected_value_task}|{selected_value_year}|{selected_value_month}|{selected_value_date}"
+    new_tasks = library.list_sort("hhh|2027|5|29", "task.txt")
+    list_tasks = tk.StringVar(value=tuple(new_tasks))
+    Listbox1 = tk.Listbox(root, listvariable=list_tasks, height=5)
+    scrollbar1 = tk.Scrollbar(root, orient=tk.VERTICAL, command=Listbox1.yview)
+    Listbox1["yscrollcommand"] = scrollbar1.set
+    Listbox1.grid(row=7, column=0)
+    scrollbar1.grid(row=7, column=1, sticky=(tk.N, tk.S))
+    print(11111111111)
+
 
 button = tk.Button(root, text="送信", command=button_clicked)
-button.grid(row=4, column=0, pady=10)
+button.grid(row=3, column=5)
 
 
 root.mainloop()
